431353f818460fad80725395a8961ad22d10779c
soloment.prod <soloment.chu@techdesign.com>
2024-01-08 13:52:47 +0800

    add new create user api part 2

--- app/core/admin.py
+++ app/core/admin.py
@@ -3,7 +3,7 @@ Django admin customization.
 """
 
 from django.contrib import admin  # noqa
-from django.contrib. auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import gettext_lazy as _
 
 from core import models
