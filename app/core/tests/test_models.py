d3ae3dedcc5fb06e5f21bb5fe8f7b2239d0ae97a
soloment.prod <soloment.chu@techdesign.com>
2024-01-19 17:46:41 +0800

    add new feature, recipe , tag, ingredient(half) feature

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,9 +1,15 @@
 """
 Test for models.
 """
+from decimal import Decimal
+
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+from core import models
+
+def create_user(email='user@example.com', password='testpass123'):
+    return get_user_model().objects.create_user(email, password)
 
 class ModelTest(TestCase):
     """Test models"""
@@ -46,3 +52,35 @@ class ModelTest(TestCase):
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
 
+    def test_create_recipe(self):
+        """Test creating a recipe is successful"""
+        user = get_user_model().objects.create_user(
+            'test@example.com',
+            'testpass123',
+        )
+        recipe = models.Recipe.objects.create(
+            user=user,
+            title="Sample recipe name",
+            time_minutes=5,
+            price=Decimal(5.0),
+            description="Sample recipe description.",
+        )
+
+        self.assertEqual(str(recipe), recipe.title)
+
+    def test_create_tag(self):
+        """Test create  a tag is successful"""
+        user = create_user()
+        tag = models.Tag.objects.create(user=user, name='Tag1')
+
+        self.assertEqual(str(tag), tag.name)
+
+    def test_create_ingredient(self):
+        """Test create an ingredient is successful"""
+        user = create_user()
+        ingredient = models.Ingredient.objects.create(
+            user=user,
+            name='Ingredient1'
+        )
+
+        self.assertEqual(str(ingredient), ingredient.name)
